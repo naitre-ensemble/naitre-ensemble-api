f390d5213aa46265108abbef10e2cdd34a55edec
Nicolas Hedoire <nicolas.hedoire@seckiot.fr>
2022-06-04 15:56:47 +0200

    make it better

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const pretty = require('express-prettify');
 const cors = require('cors');
 const helmet = require("helmet");
-// const { execQuery, execQueryWithParams } = require('./db');
+const { execQuery, execQueryWithParams } = require('./db');
 const bodyParser = require('body-parser');
 const app = express();
 
@@ -15,12 +15,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
 
-// app.post('/subscribe', async function (req, res) {
-//   const email = req.body.email;
-//   let query = `INSERT INTO contacts (email) VALUES ($1) RETURNING *`;
-//   const response = await execQueryWithParams(query, [email]);
-//   res.send(response.rows);
-// });
+app.post('/subscribe', async function (req, res) {
+  const email = req.body.email;
+  let query = `INSERT INTO contacts (email) VALUES ($1) RETURNING *`;
+  const response = await execQueryWithParams(query, [email]);
+  res.send(response.rows);
+});
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`API naitre ensemble listen on port ${process.env.PORT ? process.env.PORT : '5000'}`);
