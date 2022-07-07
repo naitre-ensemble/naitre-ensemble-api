ccb6125dc23cdec0f6fa9c9397b78b376138c3cc
Nicolas Hedoire <nicolas.hedoire@seckiot.fr>
2022-07-07 09:39:36 +0200

    add sendemail service

--- index.js
+++ index.js
@@ -30,8 +30,11 @@ app.use(bodyParser.raw());
 //     }
 // });
 
+
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // use SSL
   auth: {
     user: 'naitreensemblenord@gmail.com',
     pass: 'Funhak09'
