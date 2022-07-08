7f21b37aca15411ceea2b449d8b3cd8f3931dc52
Nicolas Hedoire <nicolas.hedoire@seckiot.fr>
2022-07-08 17:10:58 +0200

    add new feature

--- index.js
+++ index.js
@@ -25,6 +25,25 @@ const transporter = nodemailer.createTransport({
   }
 });
 
+
+const sendBonCadeauEmail = ({ email, firstname, lastname, phone, prestation, livraison, destinataire, delapart }) => {
+  // setup e-mail data, even with unicode symbols
+  const mailOptions = {
+    from: 'naitreensemblenord@gmail.fr', // sender address (who sends)
+    to: 'naitreensemble@outlook.fr', // list of receivers (who receives)
+    subject: 'Demande de réservation', // Subject line
+    text: `Demande de bon cadeau souhaitée pour "${prestation}" de  ${firstname} ${lastname}, contacter cette personne sur ce mail : ${email} ou par téléphone au ${phone}. Le destinataire est ${destinataire}. Le mode de livraison est ${livraison}. Le destinataire est ${destinataire}`, // plaintext body
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+}
+
 const sendPrestationEmail = ({ email, firstname, lastname, phone, prestation, date }) => {
   // setup e-mail data, even with unicode symbols
   const mailOptions = {
@@ -113,6 +132,20 @@ app.post('/subscribe', async function (req, res) {
   if(response[0]){
     res.send({message: 'Email registered'});
   }
+});
+
+app.post('/cadeau', async function (req, res) {
+  const email = req.body.email;
+  const firstname = req.body.firstname;
+  const lastname = req.body.lastname;
+  const phone = req.body.phone;
+  const prestation = req.body.prestation;
+  const destinataire = req.body.destinataire;
+  const livraison = req.body.livraison;
+  const delapart = req.body.delapart;
+  sendBonCadeauEmail({email, firstname, lastname, phone, prestation, destinataire, livraison, delapart });
+  res.statusCode = 200;
+  res.send({message: 'Demande envoyée'});
 });
 
 app.post('/prestation', async function (req, res) {
